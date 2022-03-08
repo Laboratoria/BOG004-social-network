c1ca4ec6899a8de01021838d61bad600ffb40425
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-03-08 18:02:39 -0500

    modificando register

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,17 +1,27 @@
 //import { firestore } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js'
-import { getAuth, createUserWhitEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
-
 import firebaseApp from '../Configfirebase/confiFirebase.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
 
 const auth = getAuth(firebaseApp);
-createUserWhitEmailAndPassword(auth, nameform, usuarioform, emailform, passwordform)
+
+async function submithandler(e) {
+  e.preventDefault();
+  const nameform = e.target.name.value;
+  const usuarioform = e.target.username.value;
+  const emailform = e.target.email.value;
+  const passwordform = e.target.password.value;
+  console.log(nameform, usuarioform, emailform, passwordform)
+
+  const users = await createUserWithEmailAndPassword(auth, nameform, usuarioform, emailform, passwordform);
+  console.log('user',users);
+}
 export const register = () => {
 
-  const template = `
+ const template = `
   <header>
   <img src='image/logo2.png' alt='social-trip-png'>
   </header>
-  <form id='formregister' class='formregister' autocomplete='off'> 
+  <form ${onsubmit=submithandler}id='formregister' class='formregister' autocomplete='off'> 
   <label for='name'>Nombres y Apellidos</label>
   <input type='name' id='name' maxlength='20' >
   <label for='username'>Nombre de Usuario</label>
@@ -20,7 +30,7 @@ export const register = () => {
   <input type='email' id='email'>
   <label for='password'>Contraseña</label>
   <input type='password' id='password' minlength='8' maxlength='16'>
-  <button type='button'>REGISTRATE</button>
+  <button type='submit' id='btnregister'>REGISTRATE</button>
   <p>Al registrarte, aceptas nuestras condiciones, la politica de datos, y la politica de cookies.</p>
   <h1>O</h1>
   <button><img src='image/botongoogle.png' alt='logo-google'>Inicia sesión con Google</button>
@@ -28,20 +38,6 @@ export const register = () => {
   // aqui tu codigo
   const divElement = document.createElement('div');
   divElement.classList.add('view2');
-  divElement.innerHTML = template;
-  const formulario = divElement.querySelector('#formregister');
-  console.log(formulario)
- 
-formulario.addEventListener('click', function onsubmit( ) {
-    const nameform = divElement.querySelector('#name').value;
-    const usuarioform= divElement.querySelector('#username').value;
-    const emailform = divElement.querySelector('#email').value;
-    const passwordform = divElement.querySelector('#password').value;      
-      console.log(nameform, usuarioform, emailform, passwordform) 
-})
+  divElement.innerHTML = template; 
   return divElement;
 };
-
-
-
-
--- src/main.js
+++ src/main.js
@@ -9,3 +9,5 @@ router(window.location.hash);
 window.addEventListener('hashchange', () => {
   router(window.location.hash);
 });
+
+
