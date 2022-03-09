3f9b4207d8b6d6a0a688104d09f4bc799fc94ff6
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-03-09 14:19:21 -0500

    integracion firebase acceso por google

--- src/main.js
+++ src/main.js
@@ -2,13 +2,33 @@
 // import { myFunction } from './lib/index.js';
 // myFunction();
 import { router } from './lib/index.js';
+import { iniciarFirebase } from './firebase.js';
+import { signInGoogle } from './auth-google.js';
+
+//firebase
+
+iniciarFirebase();
+
+//se captura el evento que avisa que se cambio la ruta y cargo la plantilla
+document.addEventListener("cambioruta", (event) => {
+    const url = event.detail.url;
+
+    if (url == '/registro') {
+        const btnSignInGoogle = document.querySelector('#btnSignInGoogle')
+        btnSignInGoogle.addEventListener('click', signInGoogle)
+    }
+
+    console.log(event)
+})
+
+
 
 // // boton de la modal
 const btnModal = document.getElementById('sign-up');
 const modalWindow = document.getElementById('modalWindow');
 btnModal.addEventListener('click', () => {
-  modalWindow.style.visibility = 'hidden';
-  router.loadRoute('registro');
+    modalWindow.style.visibility = 'hidden';
+    router.loadRoute('registro');
 });
 
 // // boton de registrar
