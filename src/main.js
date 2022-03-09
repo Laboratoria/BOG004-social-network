2be6b5b7abc243c43a87cc1ff711fde08ace5d93
Nathaly Huerta <nathaly.huertab@gmail.com>
2022-03-08 19:40:36 -0500

    router_change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 // Este es el punto de entrada de tu aplicacion
+// import { myFunction } from './lib/index.js';
+// myFunction();
+import { router } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+// // boton de la modal
+const btnModal = document.getElementById('sign-up');
+const modalWindow = document.getElementById('modalWindow');
+btnModal.addEventListener('click', () => {
+  modalWindow.style.visibility = 'hidden';
+  router.loadRoute('registro');
+});
 
-myFunction();
+// // boton de registrar
+// const btn = document.querySelector('#btn-register');
+// function funcionX() {
+//   console.log('Hola me voy a Firebase');
+// }
+// btn.addEventListener('click', funcionX());
