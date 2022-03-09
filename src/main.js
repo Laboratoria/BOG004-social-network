fa1dee91e78092664440bea45d540b950f24d9f0
Vivipe28 <viviana.perez28@gmail.com>
2022-03-09 17:47:51 -0500

    pruebas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-// Este es el punto de entrada de tu aplicacion
+import { saveTask } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+const ROUTER = new Router(PATHS)
 
-myFunction();
+let signUpbtn = document.getElementById('signUp');
+signUpbtn.addEventListener('click', () => {
+    ROUTER.load('signUp')
+})
+
+let taskFormpbtn = document.getElementById('taskForm');
+taskFormpbtn.addEventListener('click', () => {
+    ROUTER.load('taskForm')
+})
+
+const taskForm = document.getElementById('task-form');
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = taskForm['task-title'];
+    const description = taskForm['task-description'];
+
+    saveTask(title.value, description.value);
+
+    taskForm.reset();
+
+})
+
+
