552707495d6ea2861f012b9c143f03e9a8194dfe
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-03-09 21:27:53 -0500

    router

--- src/router.js
+++ src/router.js
@@ -1,42 +1,40 @@
 export default class Router {
-  constructor(routes) {
-    this.routes = routes;
-    this.loadInitialRoute();
-  }
 
-  loadRoute(...urlSegs) {
-    const matchedRoute = this.matchUrlToRoute(urlSegs);
+    constructor(rutas) {
+        this.routes = rutas;
+        this.loadInitialRoute();
+    }
 
-    const url = `/${urlSegs.join('/')}`;
-    window.history.pushState({}, 'texto', url);
+    loadRoute(urlSegs, push = true) {
+        const matchedRoute = this.matchUrlToRoute(urlSegs);
 
-    const eventoCambioRuta = new CustomEvent('cambioruta', { detail: { url } });
+        const url = `/${urlSegs}`;
 
-    const routerOutElm = document.querySelectorAll('[data-router]')[0];
-    routerOutElm.innerHTML = matchedRoute.template;
 
-    // Dispara evento personalizado 'para avisar que se cambio la ruta y se cargo la plantilla'
+        push && window.history.pushState({}, url, url);
 
-    document.dispatchEvent(eventoCambioRuta);
-  }
 
-  matchUrlToRoute(urlSegs) {
-    const matchedRoute = this.routes.find((route) => {
-      const routePathSegs = route.path.split('/').slice(1);
+        const routerOutElm = document.querySelectorAll('[data-router]')[0];
+        routerOutElm.innerHTML = matchedRoute.template;
+        matchedRoute.script()
 
-      if (routePathSegs.length !== urlSegs.length) {
-        return false;
-      }
-      return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegs[i]);
-    });
 
-    return matchedRoute;
-  }
+    }
 
-  loadInitialRoute() {
-    const pathNameSplit = window.location.pathname.split('/');
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+    matchUrlToRoute(urlSegs) {
+        return this.routes.find((ruta) =>
+            this.removeSlash(ruta.path) === urlSegs
+        )
+    }
 
-    this.loadRoute(...pathSegs);
-  }
+    loadInitialRoute() {
+        const ruta = this.removeSlash(window.location.pathname)
+        this.loadRoute(ruta, false);
+
+    }
+
+    removeSlash(path) {
+        return path.substring(1)
+    }
+
 }
--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,56 @@
-export const routes = [
-  {
-    path: '/',
-    template: `
-    <div class="modal">
-      <img class="paws-logo" src="https://res.cloudinary.com/dtaq1ip2g/image/upload/v1646690477/logo_movil_byxlxx.png" alt="paws" />
-      <p>
-          PAWS la unica red social que te permite interactuar, publicar y enterarte de las ultimas noticias sobre la proteccion y cuidado de los animales.
-      </p>
-      <div id="buttons">
-          <button id="sign-up">Registrar</button>
-          <button id="log-in" disabled>Ingresar</button>
-      </div>
-    </div>`,
-  },
-  {
-    path: '/registro',
-    template: `<div class='container-form'>
-      <form class='form' action='' method='post'>
-        <p>BIENVENIDO</p>
-        <input type='text' placeholder='Nombre y Apellido' />
-        <input type='email' placeholder='Correo' />
-        <input type='password' placeholder='Contraseña' />
-        <button type='submit' id='btn-register' >Registrarse</button>
-      </form>
-    </div>
-    <p id='btnSignInGoogle'>
-     Registrate con
-   <i class='fa-brands fa-google'></i>
-    </p>
-  `,
-  },
-  {
-    path: '/construccion',
-    template: '<h1> Pagina en construccion para Firebase</h1>',
-  },
+import { router } from './lib/index.js';
+import { signInGoogle } from './auth-google.js';
+
+export const routes = [{
+        path: '/',
+        template: `
+            <div class="modal">
+              <img class="paws-logo" src="https://res.cloudinary.com/dtaq1ip2g/image/upload/v1646690477/logo_movil_byxlxx.png" alt="paws" />
+              <p>
+                  PAWS la unica red social que te permite interactuar, publicar y enterarte de las ultimas noticias sobre la proteccion y cuidado de los animales.
+              </p>
+              <div id="buttons">
+                  <button id="sign-up">Registrar</button>
+                  <button id="log-in" disabled>Ingresar</button>
+              </div>
+            </div>`,
+        script: () => {
+            const btnModal = document.querySelector('#sign-up');
+            btnModal.addEventListener('click', () => {
+                router.loadRoute('registro');
+            });
+        }
+    },
+    {
+        path: '/registro',
+        template: `<div class='container-form'>
+                <form class='form' action='' method='post'>
+                  <p>BIENVENIDO</p>
+                  <input type='text' placeholder='Nombre y Apellido' />
+                  <input type='email' placeholder='Correo' />
+                  <input type='password' placeholder='Contraseña' />
+                  <button type='submit' id='btn-register' >Registrarse</button>
+                </form>
+              </div>
+              <p id='btnSignInGoogle'>
+              Registrate con
+            <i class='fa-brands fa-google'></i>
+              </p>
+            `,
+        script: () => {
+            const btn = document.querySelector('#btn-register');
+            btn.addEventListener('click', () => {
+                router.loadRoute('construccion');
+            });
+            const btnSignInGoogle = document.querySelector('#btnSignInGoogle');
+            btnSignInGoogle.addEventListener('click', signInGoogle);
+        },
+    },
+    {
+        path: '/construccion',
+        template: '<h1> Pagina en construccion para Firebase</h1>',
+        script: () => {
+
+        }
+    },
 ];
