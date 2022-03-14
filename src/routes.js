5a946b23e1318e703767508377eb0be2e8c594e3
Nathaly Huerta <nathaly.huertab@gmail.com>
2022-03-14 08:47:43 -0500

    Validacion formulario

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ import { router } from './lib/index.js';
 import { signInGoogle } from './lib/auth-google.js';
 import { signInEmail } from './lib/auth-email.js';
 
-export const routes = [{
-        path: '/',
-        template: `
+export const routes = [
+  {
+    path: '/',
+    template: `
             <div class='modal'>
               <img class='paws-logo' src='https://res.cloudinary.com/dtaq1ip2g/image/upload/v1646690477/logo_movil_byxlxx.png' alt='paws' />
               <p>
@@ -15,48 +16,64 @@ export const routes = [{
                   <button id='log-in' disabled>Ingresar</button>
               </div>
             </div>`,
-        script: () => {
-            const btnModal = document.querySelector('#sign-up');
-            btnModal.addEventListener('click', () => {
-                router.loadRoute('registro');
-            });
-        },
+    script: () => {
+      const btnModal = document.querySelector('#sign-up');
+      btnModal.addEventListener('click', () => {
+        router.loadRoute('registro');
+      });
     },
-    {
-        path: '/registro',
-        template: `<div class='container-form'>
-        <form class='form'>
-          <p>BIENVENIDO</p>
-          <input type='text' class='name' placeholder='Nombre y Apellido' autocomplete='off'/>
-          <input type='email' class='email' placeholder='Correo' autocomplete='off' />
-          <span id='emailOK'></span>
-          <input type='password' class='password' placeholder='Contraseña' autocomplete='off' />
-          <span id='passOK'></span>
-          <button type='button' id='btn-register' >Registrarse</button>
-        </form>
-      </div>
-      <p id='btnSignInGoogle'>
-       Registrate con
-     <i class='fa-brands fa-google'></i>
-      </p>
+  },
+  {
+    path: '/registro',
+    template: `<div class='container-form'>
+    <form action="" class='form' id='form'>
+    <p>BIENVENIDO</p>
+    <div class="form__group" id="group__name">
+    <div class="form__group-input">
+      <input type="text" class="form__input" name="nombre" id="nombre" placeholder="Nombre y Apellido">
+      <i class="form__validation-state fas fa-times-circle"></i>
+    </div>
+    <p class="form__input-error">Ingresa sólo tú name y apellido.</p>
+  </div>
+  <div class="form__group" id="group__email">
+    <div class="form__group-input">
+      <input type="email" class="form__input" name="email" id="email" placeholder="correo@email.com">
+      <i class="form__validation-state fas fa-times-circle"></i>
+    </div>
+    <p class="form__input-error">El correo sólo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+  </div>
+  <div class="form__group" id="group__password">
+    <div class="form__group-input">
+      <input type="password" class="form__input" name="password" id="password">
+      <i class="form__validation-state fas fa-times-circle"></i>
+    </div>
+    <p class="form__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
+  </div>
+    <button type='button' id='btn-register' >Registrarse</button>
+  </form>
+  </div>
+  <p id='btnSignInGoogle'>
+   Registrate con
+ <i class='fa-brands fa-google'></i>
+  </p>
             `,
-        script: () => {
+    script: () => {
+      const btn = document.querySelector('#btn-register');
+      btn.addEventListener('click', () => {
+        const email = document.querySelector('.email').value;
+        const password = document.querySelector('.password').value;
+        signInEmail(email, password);
+      });
 
-            const btn = document.querySelector('#btn-register');
-            btn.addEventListener('click', () => {
-                const email = document.querySelector('.email').value;
-                const password = document.querySelector('.password').value;
-                signInEmail(email, password)
-            });
-
-            const btnSignInGoogle = document.querySelector('#btnSignInGoogle');
-            btnSignInGoogle.addEventListener('click', signInGoogle);
-        },
+      const btnSignInGoogle = document.querySelector('#btnSignInGoogle');
+      btnSignInGoogle.addEventListener('click', signInGoogle);
     },
-    {
-        path: '/construccion',
-        template: '<h1> Pagina en construccion para Firebase</h1>',
-        script: () => {},
-    },
-
+  },
+  {
+    path: '/construccion',
+    template: `
+    <img class='img-construccion' src='https://res.cloudinary.com/dtaq1ip2g/image/upload/v1647092472/descarga_dxdene.jpg' >
+    `,
+    script: () => {},
+  },
 ];
