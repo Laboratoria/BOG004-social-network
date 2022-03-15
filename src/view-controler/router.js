a4349bb6ff56e3ce6b39e652d797e49e1d6c2ac4
Maria Saavedra <cotesaavedra@github.com>
2022-03-15 14:04:25 -0300

    creación de menú de navegación en view

--- src/view-controler/router.js
+++ src/view-controler/router.js
@@ -12,7 +12,14 @@ switch (hash) {
     { return sectionMain.appendChild(components.home()); }
     case '#/createUser': 
     { return sectionMain.appendChild(components.createUser()); }
-
+    case '#/login':
+    { return sectionMain.appendChild(components.login()); }
+    case '#/profile': 
+    { return sectionMain.appendChild(components.profile()); }
+    case '#/feed': 
+    { return sectionMain.appendChild(components.feed()); }
+    case '#/interest': 
+    { return sectionMain.appendChild(components.interest()); }
     default:
         return sectionMain.appendChild(components.different())
         
--- src/view/index.js
+++ src/view/index.js
@@ -1,13 +1,18 @@
 import home from "./home.js";
 import createUser from "./createUser.js";
+import login from "./login.js";
+import profile from "./profile.js";
+import feed from "./feed.js";
+import interest from "./interest.js";
 import different from "./404.js";
-
-
-
 
 const components = {
     home: home,
-    createUser: createUser,
+    createUser: createUser, 
+    login: login,
+    profile: profile,
+    feed: feed,
+    interest: interest,
     different: different,
 }
  
