8a4ab60bd51906574b1a4b6bdb69fbfff9a9a0d2
Nathaly Huerta <nathaly.huertab@gmail.com>
2022-03-15 15:34:26 -0500

    btn google

--- src/main.js
+++ src/main.js
@@ -4,92 +4,13 @@ import { router } from './routes.js';
 import { iniciarFirebase } from './lib/firebase.js';
 
 // firebase
-
-
-
-// validacion el form
-// const form = document.getElementById('form');
-const inputs = document.querySelectorAll('#form input');
-
-const expresiones = {
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    password: /^.{6,15}$/, // 6 a 15 digitos.
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-};
-
-const campos = {
-    name: false,
-    email: false,
-    password: false,
-
-};
-
-const validateField = (expresion, input, campo) => {
-    if (expresion.test(input.value)) {
-        document
-            .getElementById(`group__${campo}`)
-            .classList.remove('form__group-incorrect');
-        document
-            .getElementById(`group__${campo}`)
-            .classList.add('form__group-right');
-        document
-            .querySelector(`#group__${campo} i`)
-            .classList.add('fa-check-circle');
-        document
-            .querySelector(`#group__${campo} i`)
-            .classList.remove('fa-times-circle');
-        document
-            .querySelector(`#group__${campo} .form__input-error`)
-            .classList.remove('form__input-error-active');
-        campos[campo] = true;
-    } else {
-        document
-            .getElementById(`group__${campo}`)
-            .classList.add('form__group-incorrect');
-        document
-            .getElementById(`group__${campo}`)
-            .classList.remove('form__group-right');
-        document
-            .querySelector(`#group__${campo} i`)
-            .classList.add('fa-times-circle');
-        document
-            .querySelector(`#group__${campo} i`)
-            .classList.remove('fa-check-circle');
-        document
-            .querySelector(`#group__${campo} .form__input-error`)
-            .classList.add('form__input-error-active');
-        campos[campo] = false;
-    }
-};
-
-// valida la seccion de registro
-const validarForm = (e) => {
-    switch (e.target.name) {
-        case 'name':
-            validateField(expresiones.name, e.target, 'name');
-            break;
-        case 'email':
-            validateField(expresiones.email, e.target, 'email');
-            break;
-        case 'password':
-            validateField(expresiones.password, e.target, 'password');
-            break;
-        default:
-    }
-};
-
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarForm);
-    input.addEventListener('blur', validarForm);
-});
-
 window.addEventListener('hashchange', () => {
-    router.loadRoute(router.removeSlash(window.location.hash), false);
+  router.loadRoute(router.removeSlash(window.location.hash), false);
 });
 
 iniciarFirebase();
 
 window.addEventListener('popstate', () => {
-    const removePath = router.removeSlash(window.location.pathname);
-    router.loadRoute(removePath, false);
+  const removePath = router.removeSlash(window.location.pathname);
+  router.loadRoute(removePath, false);
 });
--- src/pages/registro.js
+++ src/pages/registro.js
@@ -3,8 +3,8 @@ import { signInEmail } from '../lib/auth-email.js';
 import { expresiones, validateField } from '../utils.js';
 
 export default {
-    path: '#registro',
-    template: `<div class='paws-image'>
+  path: '#registro',
+  template: `<div class='paws-image'>
   <img class='paws' src='images/animalsBackground.png' alt='paws' />
 </div>
   <div class='container-form'>
@@ -26,51 +26,52 @@ export default {
     </div>
     <div class='form__group' id='group__password'>
       <div class='form__group-input'>
-        <input type='password' class='form__input' name='password' id='password'>
+        <input type='password' class='form__input' name='password' id='password' placeholder='Contraseña'>
         <i class='form__validation-state fas fa-times-circle'></i>
       </div>
       <p class='form__input-error'>La contraseña tiene que ser de 6 a 15 dígitos alfanuméricos.</p>
   </div>
-    <button type='button' id='btn-register' >Registrarse</button>
+    <button type='button' id='btn-register'>Registrarse</button>
   </form>
-    <p id='btnSignInGoogle'>
-    Regístrate con: 
-    <img src='https://img.icons8.com/color/30/000000/google-logo.png'/>
-    </p>
+    <button id='btnSignInGoogle'>
+      <img src='https://img.icons8.com/color/30/000000/google-logo.png'/> Ingresar con Google.
+    </button>
+  
+  
   </div>
   
             `,
-    script: () => {
-        const btn = document.querySelector('#btn-register');
-        const inputs = document.querySelectorAll('#form input');
+  script: () => {
+    const btn = document.querySelector('#btn-register');
+    const inputs = document.querySelectorAll('#form input');
 
-        btn.addEventListener('click', () => {
-            const email = document.querySelector('#email').value;
-            const password = document.querySelector('#password').value;
-            signInEmail(email, password);
-        });
+    btn.addEventListener('click', () => {
+      const email = document.querySelector('#email').value;
+      const password = document.querySelector('#password').value;
+      signInEmail(email, password);
+    });
 
-        const btnSignInGoogle = document.querySelector('#btnSignInGoogle');
-        btnSignInGoogle.addEventListener('click', signInGoogle);
+    const btnSignInGoogle = document.querySelector('#btnSignInGoogle');
+    btnSignInGoogle.addEventListener('click', signInGoogle);
 
-        const validarForm = (e) => {
-            switch (e.target.name) {
-                case 'name':
-                    validateField(expresiones.name, e.target, 'name');
-                    break;
-                case 'email':
-                    validateField(expresiones.email, e.target, 'email');
-                    break;
-                case 'password':
-                    validateField(expresiones.password, e.target, 'password');
-                    break;
-                default:
-            }
-        };
+    const validarForm = (e) => {
+      switch (e.target.name) {
+        case 'name':
+          validateField(expresiones.name, e.target, 'name');
+          break;
+        case 'email':
+          validateField(expresiones.email, e.target, 'email');
+          break;
+        case 'password':
+          validateField(expresiones.password, e.target, 'password');
+          break;
+        default:
+      }
+    };
 
-        inputs.forEach((input) => {
-            input.addEventListener('keyup', validarForm);
-            input.addEventListener('blur', validarForm);
-        });
-    },
+    inputs.forEach((input) => {
+      input.addEventListener('keyup', validarForm);
+      input.addEventListener('blur', validarForm);
+    });
+  },
 };
