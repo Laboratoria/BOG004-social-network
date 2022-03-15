74b636867901146897c2884ac298f3c917af919f
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-03-15 13:26:03 -0500

    subiendo estilos basicos de vista 1,2y3

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-// Este es el punto de entrada de tu aplicacion
-
-import { router } from './lib/index.js';
-
-/* Colocamos el router antes para que cuanto arranque
-la app nos muestre nuestro inicio o home */
-
-router(window.location.hash);
-window.addEventListener('hashchange', () => {
-  router(window.location.hash);
-});
 
