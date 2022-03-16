eaeb3268cc26f0fd0d4eba364c1d25dc8fe3c656
yaraelzein <yaraelzein2001@gmail.com>
2022-03-15 21:12:38 -0500

    estilos vista register

--- src/lib/views/register.js
+++ src/lib/views/register.js
@@ -2,46 +2,54 @@ import { auth } from '../firebaseInit.js';
 import { newRegister } from '../firebaseController.js';
 
 export default () => {
-    const divRegister = document.createElement("div");
-    divRegister.setAttribute ("class", "container-div-register");
-    const viewRegister = `
-    <h1>THE DAILY PROPHET</h1>
-    <H2> WELCOME WIZARDS</H2>
-    <div class="main__div--Login">
-        <input type="text" placeholder="Email" id="email">
-        <br class="espacio">
-        <input type="text" placeholder="Password" id="password">
-        <p>REVELIO The Daily Prophet</p>
-        <a href="#/daily"><button id="join" class="join"> Join </button></a>
-        <p><hr>or<hr></p>
-        <p>Continue with Google</p><!-- Imagen e icono  -->         
-        </div>
-    <p>Already a member? ALOHOMORA</p>
-    <a href="#/login"><button href="#/login" id="login" class="Login"> Login </button></a>`
+  const divRegister = document.createElement('div');
+  divRegister.setAttribute('class', 'container-div-register');
+  const viewRegister = `
+    <main>
+    <div class="main__div--tittle">
+      <img src="./img/title.png">
+    </div>
+    <h2 class="welcome-wizards"><b> WELCOME WIZARDS</b></h2>
+    <div class="main__div--login">
+      <input type="text" placeholder="Email" id="email">
+      <br class="espacio">
+      <input type="text" placeholder="Password" id="password">
+      <p class="p_join"><b>REVELIO<b> The Daily Prophet</p>
+      <button class="join"> Join </button>
+      <div class="div-or">
+       <p class="or">or</p>
+      </div>
+      <div class="continue-with-google">
+        <p>Continue with Google</p>
+        <img src="./img/hand.png" class="hand">
+        <img src="./img/google.png"
+      </div>
+    </div>
+    <p class="alohomora">Already a member? ALOHOMORA</p>
+    <button class="Login"> Login </button>
+  </main>
+  `;
+  divRegister.innerHTML = viewRegister;
 
-divRegister.innerHTML = viewRegister;
+  const join = divRegister.querySelector('.join');
+  join.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('click');
 
-const join= divRegister.querySelector(".join");
-    
-    join.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log("click")
-
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-    console.log (auth);
-
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    console.log(auth);
     newRegister(auth, email, password)
       .then((userCredential) => {
-        //Signed in
-        console.log("Join")
+        // Signed in
+        console.log('Join');
         const user = userCredential.user;
-    })
+      })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
-    })
-  })
+      });
+  });
   return divRegister;
-}
+};
