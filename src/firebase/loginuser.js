a02b6d08d9a7742b88b60b2f8bd235a2294506c8
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-03-16 18:30:40 -0500

    login aut

--- src/firebase/loginuser.js
+++ src/firebase/loginuser.js
@@ -1,19 +1,19 @@
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
-const existingUser = (email,password) => {
-const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-        const user = userCredential.user;
-        console.log('Usuario registrado');
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.error('${error.code} ${error.message}');
-  });
 
-};
-
+function existingUser(email, password) {
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log('Login extitoso');
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // eslint-disable-next-line no-console
+      console.error(`${error.code} ${error.message}`);
+    });
+}
 export default existingUser;
