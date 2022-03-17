af289d7f12d33af6c3908c18045071f250cec420
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-03-17 09:37:57 -0500

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,33 @@
 // importamos la funcion que vamos a testear
 import { myFunction } from '../src/lib/index';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
+import Router from '../src/router.js';
+// import { routes } from '../src/routes.js';
+
+// jest.mock('../../src/lib/firebase.auth-util.js')
+
+const rutas = [{
+        path: '/',
+        template: '',
+        script: () => {
+
+        },
+    },
+    {
+        path: '/registro',
+        template: '',
+        script: () => {},
+    }
+];
+const router = new Router(rutas);
+
+describe('registro', () => {
+    it('debería Redireccionar al hash registro', () => {
+        // paso 1: Pintar la vista de registro
+        router.loadRoute('registro');
+        // paso 2: Darle valores a los input
+        // paso 3: Hacer click en el boton
+        // paso 4: Verificar que la url cambie
+        expect(window.location.hash).toBe('registro');
+    });
 });
