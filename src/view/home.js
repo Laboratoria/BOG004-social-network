248b9fce8b9f3954ac944392c3d184ca738db953
Maria Saavedra <cotesaavedra@github.com>
2022-03-18 10:05:33 -0300

    template home y css

--- src/view/home.js
+++ src/view/home.js
@@ -1,6 +1,12 @@
+// import imagen from "./logo-nibbles.png";
 export default () => {
+    // console.log(imagen);
     const viewHome = `
- <h2 class="text.center"> ¡Hola! ingresa o regístrate en Nibbles y descubre recetas saludables</h2>`;
+    <img src="logo-nibbles.png" alt="logo-nibbles"/>
+    <button id="loginBtn">Ingresa</button>
+    <button id="createUserBtn">Crear cuenta</button>
+    <p>Conéctate con</p>
+    <img src="google.png" alt="ingresa-con-google">`;
  const divElem = document.createElement("div")
  divElem.innerHTML = viewHome
  divElem.classList.add("position")
