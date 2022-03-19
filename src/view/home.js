3ace62f47abf3e4468b0575a627ce91a3eea64db
Maria Saavedra <cotesaavedra@github.com>
2022-03-19 11:10:51 -0300

    agregar funcionalidad a boton login, CSS a Home y creación del contenido de Home

--- src/view/home.js
+++ src/view/home.js
@@ -1,14 +1,36 @@
-// import imagen from "./logo-nibbles.png";
+
 export default () => {
-    // console.log(imagen);
-    const viewHome = `
-    <img src="logo-nibbles.png" alt="logo-nibbles"/>
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    const leftColumn = document.createElement("div");
+    leftColumn.classList.add("leftColumn");
+    const viewLeft = `
+    <img id="homeImg" src="../img/home-img.png" alt="boot_image">
+    `;
+    leftColumn.innerHTML = viewLeft;
+    container.appendChild(leftColumn);
+
+    const rightColumn = document.createElement("div");
+    rightColumn.classList.add("rightColumn");
+    const viewRight = `
+    <img id="nibblesLogo" src="../img/nibblesLogo.png" alt="logo-nibbles">
     <button id="loginBtn">Ingresa</button>
     <button id="createUserBtn">Crear cuenta</button>
     <p>Conéctate con</p>
-    <img src="google.png" alt="ingresa-con-google">`;
- const divElem = document.createElement("div")
- divElem.innerHTML = viewHome
- divElem.classList.add("position")
- return divElem;
+    <hr class="divider">
+    <img class="googleLogo "src="../img/google.png" alt="ingresa-con-google">`;
+    rightColumn.innerHTML = viewRight;
+    container.appendChild(rightColumn);
+
+    const loginBtn = container.querySelector("#loginBtn");  
+    loginBtn.addEventListener("click", () => {
+        console.log("hola");
+        location.hash = "#/login";
+    });
+    const GooglenBtn = container.querySelector("#googleLogo");  
+    loginBtn.addEventListener("click", () => {
+     console.log("hola");
+    })
+ return container;
 }
