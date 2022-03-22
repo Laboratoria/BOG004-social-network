3bb4192a335f157b6b54ad27f012d5e1f6cd4572
Maria Saavedra <cotesaavedra@github.com>
2022-03-22 18:35:17 -0300

    Creación del contenido de Home, Login y Register,además de la autenticación

--- src/view/index.js
+++ src/view/index.js
@@ -1,14 +1,14 @@
 import home from "./home.js";
-import createUser from "./createUser.js";
+import { createFormUser, saveUser } from "./user.js";
 import login from "./login.js";
 import profile from "./profile.js";
 import feed from "./feed.js";
 import interest from "./interest.js";
 import different from "./404.js";
-
+ 
 const components = {
     home: home,
-    createUser: createUser, 
+    user: { createFormUser, saveUser },
     login: login,
     profile: profile,
     feed: feed,
