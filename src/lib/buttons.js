8b647086f4753eab8b977f0af588176ab27de37f
unknown <accscortes09@gmail.com>
2022-03-22 16:13:54 -0500

    Corrigiendo el Eslint de todos los archivos

--- src/lib/buttons.js
+++ src/lib/buttons.js
@@ -1,33 +1,37 @@
 import { googleSignWithPopup } from './firebase.js';
+// eslint-disable-next-line import/no-cycle
 import { changeView } from './viewController.js';
+
 export default () => {
-    const viewButtons = `
-<div class="initView" id="initView">
+  const viewButtons = `
+<div class='initView' id='initView'>
  <figure>
-        <img class="Icono" src="images/iPhone 13/Logo.png" alt="Icono"><br>
-        <img class="Nave" src="./images/Nave.png" alt="Nave">
+        <img class='Icono' src='images/iPhone 13/Logo.png' alt='Icono'><br>
+        <img class='Nave' src='./images/Nave.png' alt='Nave'>
 </figure>
-    <h1 class="branding">EDUCATION IS THE FUTURE <h1><br>
-    <a class="signUp" href="#/signUp">SIGN UP</a><br>
-    <a class="buttonGoogle"  id="loginGoogle">CONTINUE WITH GOOGLE</a> <br>
-    <a class="signIn" href="#/signIn">SIGN IN</a>
+    <h1 class='branding'>EDUCATION IS THE FUTURE <h1><br>
+    <a class='signUp' href='#/signUp'>SIGN UP</a><br>
+    <a class='buttonGoogle'  id='loginGoogle'>CONTINUE WITH GOOGLE</a> <br>
+    <a class='signIn' href='#/signIn'>SIGN IN</a>
 </div>
 <footer> Made by Viviana, Camila & Paula</footer>
-`
-    const sectionElement = document.createElement('div')
-    sectionElement.innerHTML = viewButtons;
+`;
+  const sectionElement = document.createElement('div');
+  sectionElement.innerHTML = viewButtons;
 
-    const googleButton = sectionElement.querySelector('#loginGoogle');
-    googleButton.addEventListener('click', () => {
-
-        googleSignWithPopup()
-            .then(() => { changeView('#/post') })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                alert(errorMessage)
-            })
-
-    })
-    return sectionElement;
-}
+  const googleButton = sectionElement.querySelector('#loginGoogle');
+  googleButton.addEventListener('click', () => {
+    googleSignWithPopup()
+      .then(() => {
+        changeView('#/post');
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-unused-vars
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // eslint-disable-next-line no-alert
+        alert(errorMessage);
+      });
+  });
+  return sectionElement;
+};
--- src/lib/post.js
+++ src/lib/post.js
@@ -1,36 +1,34 @@
 import { saveTask, userSignOut } from './firebase.js';
+// eslint-disable-next-line import/no-cycle
 import { changeView } from './viewController.js';
 
 export default () => {
-    const post = `
-<form id="task-form">
-<label for="title">Paula</label>
+  const post = `
+<form id='task-form'>
+<label for='title'>Paula</label>
 
-<textarea id="task-description" rows="3" placeholder="Task description"></textarea>
-<button id="btn_task_save">Publish!</button>
-<button id="logout">Log out</button>
-</form>`
+<textarea id='task-description' rows='3' placeholder='Task description'></textarea>
+<button id='btn_task_save'>Publish!</button>
+<button id='logout'>Log out</button>
+</form>`;
 
-    const taskContainer = document.createElement('div');
-    taskContainer.innerHTML = post;
-    const taskForm = taskContainer.querySelector('#task-form');
-    console.log(taskForm);
-    taskForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const title = document.getElementById('task-title');
-        const description = document.getElementById('task-description');
-        saveTask(title.value, description.value);
-        taskForm.reset();
+  const taskContainer = document.createElement('div');
+  taskContainer.innerHTML = post;
+  const taskForm = taskContainer.querySelector('#task-form');
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('task-title');
+    const description = document.getElementById('task-description');
+    saveTask(title.value, description.value);
+    taskForm.reset();
+  });
 
-    })
-
-    const buttonSignOut = taskContainer.querySelector('#logout');
-    buttonSignOut.addEventListener('click', (e) => {
-        e.preventDefault();
-        userSignOut()
-        .then(() => {changeView('#/home')});
-    })
-    return taskContainer;
-    
-
-}
+  const buttonSignOut = taskContainer.querySelector('#logout');
+  buttonSignOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    userSignOut().then(() => {
+      changeView('#/home');
+    });
+  });
+  return taskContainer;
+};
