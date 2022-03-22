b8a2892a496d9d6e4b412cbc17e4e293db2c98d8
Paola Huyo <di.paolahuyo@gmail.com>
2022-03-22 17:48:30 -0500

    Separamos las funcionalidades de firebase por archivos js en la carpeta firebase, funciona el evento con el console log en el evento de Registrar Cuenta en registro.js

---
+++ src/Firebase/firebase-import.js
@@ -0,0 +1,2 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,3 @@
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-analytics.js";
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBYQRWOJCKW_FSQzdmKwZBZ6Bl3hq6ZZfo",
-//   authDomain: "ninja-social-network.firebaseapp.com",
-//   projectId: "ninja-social-network",
-//   storageBucket: "ninja-social-network.appspot.com",
-//   messagingSenderId: "831935966918",
-//   appId: "1:831935966918:web:1d6c8a571c41c911bf31c7",
-//   measurementId: "G-C0G4TLFDJK"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 //Importamos la funcion de routeo
 import { changeView } from './view-controler/controler.js';
 const start = () => {
