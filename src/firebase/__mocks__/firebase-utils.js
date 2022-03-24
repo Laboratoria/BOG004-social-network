6d4c2d894132fdbf5d107180ca8e06be16b8a62a
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-03-23 19:59:16 -0500

    mockup firestore

---
+++ src/firebase/__mocks__/firebase-utils.js
@@ -0,0 +1,6 @@
+export const getAuth = () => Promise.resolve({});
+export const createUserWithEmailAndPassword = () => Promise.resolve({});
+export const signInWithEmailAndPassword = () => Promise.resolve({});
+export const onAuthStateChanged = () => Promise.resolve({});
+export const signInWithPopup = () => Promise.resolve({});
+export const GoogleAuthProvider = () => Promise.resolve({});
