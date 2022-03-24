7a08386a8116b00545f7199cf7104da7101ed670
Nataly Escobar <natalyescobar85@gmail.com>
2022-03-24 10:50:16 -0500

    mock for createUser

--- src/firebase/__mocks__/firebase-utils.js
+++ src/firebase/__mocks__/firebase-utils.js
@@ -1,6 +1,14 @@
 export const getAuth = () => Promise.resolve({});
-export const createUserWithEmailAndPassword = () => Promise.resolve({});
+export const createUserWithEmailAndPassword = (auth, email, password) => new Promise(
+  (resolve, reject) => {
+    if (email === 'juanchito@gmail.com') {
+      reject(new Error('usuario existente'));
+    } else {
+      resolve();
+    }
+  },
+);
 export const signInWithEmailAndPassword = () => Promise.resolve({});
 export const onAuthStateChanged = () => Promise.resolve({});
 export const signInWithPopup = () => Promise.resolve({});
-export const GoogleAuthProvider = () => Promise.resolve({});
+export const GoogleAuthProvider = () => { Promise.resolve({}); };
