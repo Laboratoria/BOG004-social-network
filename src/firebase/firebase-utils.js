8770252953aeee76db8e8011968ba6d798bd0e8e
Nataly Escobar <natalyescobar85@gmail.com>
2022-03-24 18:28:25 -0500

    mocks test createUser

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -1,6 +1,7 @@
 import {
   getAuth, createUserWithEmailAndPassword,
   signInWithEmailAndPassword, onAuthStateChanged, signOut, signInWithPopup, GoogleAuthProvider,
+// eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
 
 export {
--- test/firebase/controlerfirebase.spec.js
+++ test/firebase/controlerfirebase.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-promise-executor-return */
 import { createUser } from '../../src/firebase/controlerfirebase.js';
 
 jest.mock('../../src/firebase/firebase-utils.js');
@@ -10,14 +11,14 @@ describe('createUser', () => {
   it('Deberia permitir la creacion de un usuario', async () => {
     document.body.innerHTML = '<div id="id-message-error-record"></div>';
     createUser('ferchito@gmail.com', 'miPasswordSeguro');
-    await new Promise(r => setTimeout(r, 2000));//Espera 2 segundos
+    await new Promise((r) => setTimeout(r, 2000));// Espera 2 segundos
     expect(document.getElementById('id-message-error-record').innerHTML).toBe('');
   });
-  
+
   it('Deberia arrojar un error en la crecion', async () => {
     document.body.innerHTML = '<div id="id-message-error-record"></div>';
     createUser('juanchito@gmail.com', 'miPasswordSeguro');
-    await new Promise(r => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, 2000));
     expect(document.getElementById('id-message-error-record').innerHTML).not.toBe('');
   });
 });
