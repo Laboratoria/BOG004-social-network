fa1f026089efb7dd47cc749cea2b2c8568a9ef09
NanaValencia <diseno.1988@gmail.com>
2022-03-25 11:06:40 -0500

    Creamos alerts para campos vacíos y correos nos correspondientes, adaptamos la fecha de nacimiento con formato

--- src/views/createAccount.js
+++ src/views/createAccount.js
@@ -11,7 +11,7 @@ export default () => {
       <div class='gridBodyAccount'>
         <input class='inputStyle' id='inputName' type='text' placeholder='Nombre y Apellido'>
         <input class='inputStyle' id='inputE' type='email' placeholder='Correo Electrónico'>
-        <input class='inputStyle' id='inputBirthdate' type='text' placeholder='Fecha de Nacimiento'>
+        <input class='inputStyle' id='inputBirthdate' type='text' placeholder='Fecha de Nacimiento' onfocus="(this.type='date')">
         <input class='inputStyle' id='inputPassword' type='password' placeholder='Contraseña'>
         <input class='inputStyle' id='inputConfirm' type='password' placeholder='Confirmar Contraseña'>
         <button class='btnRegister' id='viewWall'><a href='#/ecoTraveler'>Regístrate</a></button>
@@ -24,12 +24,22 @@ export default () => {
   const btnViewRegister = divAccount.querySelector('#viewWall');
   // creamos un evento al boton registrate
   btnViewRegister.addEventListener('click', () => {
+    const userName = document.getElementById('inputName').value;
+    const birthDate = document.getElementById('inputBirthdate').value;
+    const passwordConfirm = document.getElementById('inputConfirm').value;
+    console.log(userName);
     /* creamos 2 constantes para capturar los datos ingresado
     por el usuario de email y contraseña en los inputs */
     const email = document.getElementById('inputE').value;
     const password = document.getElementById('inputPassword').value;
+    if (userName, birthDate, passwordConfirm, email, password.trim() == '') {
+      alert ('Debes ingresar un valor en el campo');
+    }
+    if (password != passwordConfirm) {
+      alert ('Tus contraseñas no coinciden, intentalo de nuevo');
+    }
     /* llamamos la funcion createUser con los argumentos de email
-  y contraseñas guardados en las constantes anteriores */
+    y contraseñas guardados en las constantes anteriores */
     createUser(email, password);
     console.log('Saludo');
   });
