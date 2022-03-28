54f8bd55b5f246072ffda5dc7ed46ffa0695313c
Paola Huyo <di.paolahuyo@gmail.com>
2022-03-28 15:30:35 -0500

    Se deja listo el css para register y login

--- src/view/login.js
+++ src/view/login.js
@@ -6,28 +6,32 @@ export const login = () => {
     <div id="viewLogin">
         <a href="#/login"></a>
         <div>
-            <img id="logoRes-signUp" src="/images/logo-ninja-signup-responsive.svg">
+            <img id="logoRes-signUp" src="/images/logo-ninja-signup-responsive.png">
         </div>
         <div class="box-form">
             <form id="form-login" action="">
+              <div class="box-input">
                 <label class="labels" for="email">Ingresa tu correo registrado</label>
                 <input class="inputs" id= "email" type="email" placeholder="ejemplo@gmail.com" required>
+              </div>
+              <div class="box-input">
                 <label class="labels" for="password">Contraseña</label>
                 <input class="inputs" id= "password" type="password" required>
-                <section id="alertMessageLogin" >
-                  <p id="errorMessageLogin">Mensaje de alerta</p>
-                </section>
-                <section class="align-buttons">
-                    <div class="question-button">
-                        <p>¿Eres un usuario nuevo?</p>
-                        <button id="btn-register-signUp" class="btn-border">REGÍSTRATE</button>
-                    </div>
-                    <button id="btn-feed" class="btn-background" type="submit">INGRESAR</button>
-                </section>
+              </div>
+              <section id="alertMessageLogin" class="center-message">
+                <p id="errorMessageLogin">Mensaje de alerta</p>
+              </section>
+              <section class="align-buttons">
+                <p id="guide-button-signUp">¿Eres un usuario nuevo?</p>
+                <button id="btn-register-signUp" class="btn-border">REGÍSTRATE</button>
+                <button id="btn-feed" class="btn-background" type="submit">INGRESAR</button>
+              </section>
+              <section id="signUp-google">
                 <h3>o ingresa con: </h3> 
                 <a href=""><img class="btn-icon" src="/images/simbolo-de-google.png" alt="Google"></a>
+              </section>
             </form>
-        </div>  
+        </div>            
     </div>
     `;
   viewLoginHtml.innerHTML = view;
