de040a69f1cfdb54d9067e95bf4dcc43dc667e7c
Nataly Escobar <natalyescobar85@gmail.com>
2022-03-27 20:37:41 -0500

    Increase test coverage

--- test/firebase/controlerfirebase.spec.js
+++ test/firebase/controlerfirebase.spec.js
@@ -12,48 +12,54 @@ describe('createUser', () => {
   it('Deberia permitir la creacion de un usuario', async () => {
     document.body.innerHTML = '<div id="id-message-error-record"></div>';
     createUser('ferchito@gmail.com', 'miPasswordSeguro');
-    await new Promise((r) => setTimeout(r, 2000));// Espera 2 segundos
+    await new Promise((r) => { setTimeout(r, 2000); });// Espera 2 segundos
     expect(document.getElementById('id-message-error-record').innerHTML).toBe('');
   });
 
   it('Deberia arrojar un error en la crecion', async () => {
     document.body.innerHTML = '<div id="id-message-error-record"></div>';
     createUser('juanchito@gmail.com', 'miPasswordSeguro');
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => { setTimeout(r, 2000); });// Espera 2 segundos
     expect(document.getElementById('id-message-error-record').innerHTML).not.toBe('');
   });
 });
 
 describe('existingUser', () => {
   it('debería ser una función', () => {
-    expect(typeof createUser).toBe('function');
+    expect(typeof existingUser).toBe('function');
   });
   it('Deberia permitir el ingreso de un usuario', async () => {
     document.body.innerHTML = '<div id="id-message-error-login"></div>';
     existingUser('ferchito@gmail.com', 'miPasswordSeguro');
-    await new Promise ((r) => setTimeout((r), 2000));// Espera 2 segundos
+    await new Promise((r) => { setTimeout(r, 2000); });// Espera 2 segundos
     expect(document.getElementById('id-message-error-login').innerHTML).toBe('');
   });
 
   it('Deberia arrojar un error en el ingreso', async () => {
     document.body.innerHTML = '<div id="id-message-error-login"></div>';
     existingUser('hola', 'mi');
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => { setTimeout(r, 2000); });// Espera 2 segundos
     expect(document.getElementById('id-message-error-login').innerHTML).not.toBe('');
   });
-  describe('observerUserState', () => {
-    it('debería ser una función', () => {
-      expect(typeof observerUserState).toBe('function');
-    });
-    describe('signInWithGoogle', () => {
-      it('debería ser una función', () => {
-        expect(typeof signInWithGoogle).toBe('function');
-      });
-      describe('rcloseSession', () => {
-        it('debería ser una función', () => {
-          expect(typeof closeSession).toBe('function');
-        });
-      });
-    });
+});
+
+describe('observerUserState', () => {
+  it('debería ser una función', () => {
+    expect(typeof observerUserState).toBe('function');
+    observerUserState();
+  });
+});
+
+describe('signInWithGoogle', () => {
+  it('debería ser una función', () => {
+    expect(typeof signInWithGoogle).toBe('function');
+    signInWithGoogle();
+  });
+});
+
+describe('closeSession', () => {
+  it('debería ser una función', async () => {
+    expect(typeof closeSession).toBe('function');
+    closeSession();
   });
 });
