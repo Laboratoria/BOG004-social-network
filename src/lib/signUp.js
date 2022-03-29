e2c39436ed8983270671c07d727a4ed3e8dfafc6
unknown <accscortes09@gmail.com>
2022-03-29 12:31:27 -0500

    Agregando verificación del correo para vista SignUp

--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -12,15 +12,14 @@ export default () => {
     <h1>SIGN UP</h1>
     <input type="text" class="nickname" id="nickname" placeholder="nickname" autocomplete = 'off'><br>
     <input type="email" class="email" id="email" placeholder="email" required minlength="8"> <br>
-    <span id="emailError"></span>
     <input type="password" class="password" id="password" placeholder="password"><br>
-    <span id="passwordError"></span>
+    <span id="verificationError"></span>
     <h6>  By clicking Agree & Join, you agree to the  User Agreement, <br>
           Privacy Policy, and Cookie Policy. 
     </h6>
     <input type="submit" class="submitSignUp" value="Sign Up">
 
-</form>`;
+    </form>`;
 
   const signUpContainer = document.createElement('div');
   signUpContainer.innerHTML = viewSignUp;
@@ -30,23 +29,20 @@ export default () => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const emailError = document.getElementById('emailError');
-    const passwordError = document.getElementById('passwordError');
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
-    if (!email.includes('@') || !email.includes('.')) {
-      emailError.innerHTML = 'Debe incluir un \'@\' y un \'.\'';
-    }
-    if (regex.test(password)) {
+    const verificationError = document.getElementById('verificationError');
+    const regexDominio = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
+    if (regexDominio.test(email)) {
       SignUpUser(email, password)
         .then(() => { changeView('#/post'); })
         .catch((error) => {
         // eslint-disable-next-line no-unused-vars
           const errorCode = error.code;
-          const errorMessage = error.message;
-          alert(errorMessage);
+          if (errorCode) {
+            verificationError.innerHTML = 'Usuario ya está registrado';
+          }
         });
     } else {
-      passwordError.innerHTML = 'La contraseña debe tener de 8 a 15 caracteres, al menos una mayúscula, una minúscula, un caracter especial y no debe tener espacios';
+      verificationError.innerHTML = 'Correo inválido';
     }
   });
   return signUpContainer;
