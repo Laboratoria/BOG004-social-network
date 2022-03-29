53bfd5ffc5e325c16f2918d33b7f16bd3382dd9e
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-03-29 15:38:49 -0500

    firestore instal

--- src/views/wall.js
+++ src/views/wall.js
@@ -4,17 +4,14 @@ export default () => {
     <div id="wall-content">
     <div id="editPost">
       <textarea id="thinksuseredit" rows="4" cols="50" placeholder="¿Que estas pensado?..."></textarea>
-      <button class="btn-post">Publicar</button>
+      <button id="toPost" class="btn-post">Publicar</button>
       </div>
     <div class="post">
       <img src="" alt="" class="profile">
       <div class="postcontent">
-        <h1 class="postuser">Madrescrianza @madrescrianza</h1>
-        <p class="thinksuser">
-    // eslint-disable-next-line max-len
-          A veces se nos olvida que nuestros hijos están escuchando
-          y ellos estan absorbiendo como esponjas todo lo que decimos y hacemos.
-        </p>
+        <div id="user-thinking" class="postuser"></div>
+        <div id="space-thinking" class="thinksuser">
+        </div>
       <div class="posticons">
         <img src="img/moms.jpeg" alt="like-moms" class="posticon">
         <img src="img/comment.jpeg" alt="click-comment" class="posticon">
