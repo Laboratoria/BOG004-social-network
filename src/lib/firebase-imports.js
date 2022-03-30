4720bf047d6fd7a60092e14a945c8685c5b2714f
Vivipe28 <viviana.perez28@gmail.com>
2022-03-29 23:57:52 -0500

    Test IN/UP y estilización

--- src/lib/firebase-imports.js
+++ src/lib/firebase-imports.js
@@ -1,13 +1,22 @@
 export {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    sendEmailVerification,
-    signInWithPopup,
-    GoogleAuthProvider,
-    signOut,
-  
-  } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
-  export { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
-  export { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
-  export { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-database.js";
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
+export {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
+export{ getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-database.js';
+export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -1,6 +1,6 @@
 import { SignUpUser } from './firebase.js';
+import { changeView } from './viewController.js'
 // eslint-disable-next-line import/no-cycle
-import { changeView } from './viewController.js';
 
 export default () => {
   const viewSignUp = ` <form class="signUpForm" id="signUpForm">
@@ -10,13 +10,13 @@ export default () => {
     </figure>
     <h1>SIGN UP</h1>
     <input type="text" class="nickname" id="nickname" placeholder="nickname" autocomplete = 'off'><br>
-    <input type="email" class="email" id="email" placeholder="email" required minlength="8"> <br>
-    <input type="password" class="password" id="password" placeholder="password"><br>
+    <input type="email" class="email" id="emailSignUp" placeholder="email" required minlength="8"> <br>
+    <input type="password" class="password" id="passwordSignUp" placeholder="password"><br>
     <h6>  By clicking Agree & Join, you agree to the  User Agreement, <br>
           Privacy Policy, and Cookie Policy. 
     </h6>
-    <input type="submit" class="submitSignUp" value="Sign Up">
-
+    <input type="submit" class="submitSignUp" value="Sign Up"><br>
+    <span class = 'errorZ'></span>
 </form>`;
 
   const signUpContainer = document.createElement('div');
@@ -25,15 +25,25 @@ export default () => {
   const formSignUp = signUpContainer.querySelector('#signUpForm');
   formSignUp.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = signUpContainer.querySelector('#emailSignUp').value;
+    const password = signUpContainer.querySelector('#passwordSignUp').value;
+    const errorM = signUpContainer.querySelector('.errorZ');
+    const regexDominio = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
+    
+    
+    if (regexDominio.test(email)) { 
     SignUpUser(email, password)
-      .then(() => { changeView('#/post'); })
+      .then(() => { changeView('#/signIn'); })
       .catch((error) => {
         const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(errorMessage);
+        if(errorCode){
+          errorM.innerHTML = 'Usuario ya existente';
+        };
       });
+  }
+      else {
+        errorM.innerHTML = 'Correo inválido'
+      }
   });
   return signUpContainer;
 };
