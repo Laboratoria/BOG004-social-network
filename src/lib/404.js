ff51d2965af41676e2026c04785224b5d107abb3
LauraCastellanos13 <lauracastellanosym@gmail.com>
2022-03-30 12:18:39 -0500

    Navegation bar, html and timline design

--- src/lib/404.js
+++ src/lib/404.js
@@ -1,7 +1,28 @@
 export default () => {
   const viewError = `
-        <h1>404</h1>
-        <h>Page Not Found</h1>`;
+  <h1>404</h1>
+  <h>Page Not Found</h1>;
+      <input id="toggle" type="checkbox"></input>  
+      <label for="toggle" class="hamburger">
+          <div class="top-bun"></div>
+          <div class="meat"></div>
+          <div class="bottom-bun"></div>
+        </label>
+        <div class="nav">
+          <div class="nav-wrapper">
+            <nav>
+              <ul class="list-bar" style="list-style-type: none;">
+                <li id="homie"><a href="#/" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >PetShare</a>
+                  </li>
+                <li id="in"><a href="#/signIn" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >Sign In</a>
+                  </li>
+                <li id="up"><a href="#/signUp" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >Sign Up</a>
+                  </li>
+              </ul>
+            </nav>
+          </div>
+        </div> 
+      `;
 
   const divElement = document.createElement("div");
   divElement.setAttribute("id", "message");
--- src/lib/signIn.js
+++ src/lib/signIn.js
@@ -2,6 +2,26 @@ import { userSignIn, googleLogIn } from '../firebase.js';
 
 export default () => {
   const viewSignIn = `
+      <input id="toggle" type="checkbox"></input>  
+      <label for="toggle" class="hamburger">
+          <div class="top-bun"></div>
+          <div class="meat"></div>
+          <div class="bottom-bun"></div>
+        </label>
+        <div class="nav">
+          <div class="nav-wrapper">
+            <nav>
+              <ul class="list-bar" style="list-style-type: none;">
+                <li id="homie"><a href="#/" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >PetShare</a>
+                  </li>
+                <li id="in"><a href="#/signIn" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >Sign In</a>
+                  </li>
+                <li id="up"><a href="#/signUp" style="color: white;" onmouseover="this.style.color='rgb(177, 240, 76)';" onmouseout="this.style.color='white';" >Sign Up</a>
+                  </li>
+              </ul>
+            </nav>
+          </div>
+        </div> 
   <div class="container">
     
         <label for="umail"><b>UserEmail</b></label>
