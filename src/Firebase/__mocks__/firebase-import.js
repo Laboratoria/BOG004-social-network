e0d70f57b4fa7cdd2eb1d5efa16d632fc9e1b1c2
yuliduquedealba <yduquedealba@gmail.com>
2022-03-31 16:37:33 -0500

    Se realiza test para cuando el usuario no logra registrarse correctamente junto a mariano

--- src/Firebase/__mocks__/firebase-import.js
+++ src/Firebase/__mocks__/firebase-import.js
@@ -1,6 +1,6 @@
 export const initializeApp = () => Promise.resolve({});
-export const getAuth = () => Promise.resolve({});
-export const createUserWithEmailAndPassword = () => Promise.resolve({});
+export const getAuth = () => ({ _id: 'get-auth' });
+export const createUserWithEmailAndPassword = jest.fn(() => Promise.resolve({}));
 export const signInWithEmailAndPassword = () => Promise.resolve({});
 // export const onAuthStateChanged = ()
 export const getFirestore = () => Promise.resolve({});
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,25 +2,33 @@ import { registerUser } from "../src/Firebase/auth.js";
 import { register } from "../src/view/register.js";
 // importamos la funcion que vamos a testear
 
+import { createUserWithEmailAndPassword } from '../src/Firebase/firebase-import.js';
+
 jest.mock('../src/Firebase/firebase-import.js')
 
 describe('registerUser', () => {
     it('debería ser una función', () => {
         expect(typeof registerUser).toBe('function');
     });
-    it('Debería permitir el registro del usuario', () => {
-        document.body.innerHTML = '<p id="errorMessage"></p>';
-        registerUser('holaholaquetal@gmail.com', 'contraseña123');
-        expect(document.getElementById('errorMessage').innerHTML).toBe('');
+
+    //Esta no está testeando absolutamente nada :)
+    it('Debería permitir el registro del usuario', (done) => {
+        document.body.innerHTML = '<p id="errorMessage">error por defecto</p>';
+        registerUser('holaholaquetal@gmail.com', 'contraseña123')
+            .then(() => {
+                expect(document.getElementById('errorMessage').innerHTML).toBe('');
+                done();
+            })
     });
-    // it('El usuario no puede registrarse correctamente', () => {
-    //     document.body.innerHTML = '<p id="errorMessage"></p>';
-    //     registerUser('holaholaquetal@gmail.com', 'contraseña123');
-    //     expect(document.getElementById('errorMessage').innerHTML).not.toBe("");
-    // });
-    it('El usuario no puede registrarse correctamente', () => {
+
+    it.only('El usuario no puede registrarse correctamente', (done) => {
         document.body.innerHTML = '<p id="errorMessage"></p>';
-        registerUser('holahol34@gmail.com', 'contr');
-        expect(document.getElementById('errorMessage').innerHTML).toBe("La contraseña debe tener mínimo 6 caracteres");
+        registerUser('holahol34@gmail.com', 'contr')
+            .then(() => {
+                console.log(createUserWithEmailAndPassword.mock.calls[0])
+                expect(createUserWithEmailAndPassword.mock.calls[0])
+                    .toEqual([{ _id: 'get-auth' }, 'holahol34@gmail.com', 'contr'])
+                done();
+            })
     });
 });
