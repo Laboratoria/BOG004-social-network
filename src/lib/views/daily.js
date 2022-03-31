37bc94cc21f77f33d92289cba9b1115f2fcfb5e2
AnamariaMC <anymedinac14@gmail.com>
2022-03-31 12:12:19 -0500

    Creamos el modal para hacer los post

--- src/lib/views/daily.js
+++ src/lib/views/daily.js
@@ -10,31 +10,41 @@ export default () => {
     <img id='Banner_img' src='./img/title.png'>
   </header>
   <main>
-    <form id='modal_post-container' class="post-container">
-      <div id='modal_header'>
-        <img id='user_img' src='./img/Icono_Harry.png'>
-        <div id='name-container'></div>
-      </div>
-      <div id='line'>
-        <div id='text-container'>
-          <textarea type='text' id='post-description' placeholder='Reveal your secrets'></textarea>
+    <div id='modal-background'>
+      <form id='modal_post-container' class="post-container">
+        <div id='modal_header'>
+          <img id='user_img' src='./img/Icono_Harry.png'>
+          <div id='name-container'></div>
         </div>
-      </div>
-      <button disabled type='button' id='btn-post' class='btn-post-inactive'></button>  
-    </form>
+        <div id='line'>
+          <div id='text-container'>
+            <textarea type='text' id='post-description' placeholder='Reveal your secrets'></textarea>
+          </div>
+        </div>
+        <button type='button' id='btn-post-save' class='btn-post-inactive'>save</button>  
+      </form>
+    </div>
     <div id='post-container' class="post-container">        
     </div>       
   </main>
   <footer id='create-post'>
-    <button type='button' id='btn-post-save'>Save +</button> 
+    <button type='button' id='btn-post-create'>create +</button> 
   </footer>
   `;
   divDaily.innerHTML = viewDaily;
 
-  const modalForm = divDaily.querySelector('#modal_post-container');
-  const btnCreate = divDaily.querySelector('#btn-post-save');
+  const btnCreate = divDaily.querySelector('#btn-post-create');
 
-  btnCreate.addEventListener('click', (e) => {
+  btnCreate.addEventListener('click', () => {
+    document.querySelector('#modal-background').style.display = 'flex';
+    document.querySelector('#modal_post-container').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  })
+
+  const modalForm = divDaily.querySelector('#modal_post-container');
+  const btnSave = divDaily.querySelector('#btn-post-save');
+
+  btnSave.addEventListener('click', (e) => {
     e.preventDefault()
     // console.log('Created');
 
