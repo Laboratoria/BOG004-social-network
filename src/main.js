5ebbc4271574538a3af45a69ba2e59dccd96afe2
Vivipe28 <viviana.perez28@gmail.com>
2022-04-01 12:45:03 -0500

    Cambios CRUD

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { changeView } from './lib/viewController.js';
 const init = () => {
     window.addEventListener('hashchange', () => changeView(window.location.hash))
 }
-window.addEventListener('load', init)
+window.addEventListener('load', init);
+
+
 
 
 
