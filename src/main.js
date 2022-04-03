4974c5e08e67b6f8c2b7db81607e55f5b391f6e3
FRANCY15 <90322713+FRANCY15@users.noreply.github.com>
2022-04-03 17:20:55 -0500

    Revert "Release"

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
-// import { myFunction } from './lib/index.js';
+import { myFunction } from './lib/index.js';
 
-// myFunction();
-
-import { changeTmp } from './view-controller/router.js';
-
-const init = () => {
-    changeTmp(window.location.hash);
-    window.addEventListener('hashchange', () => changeTmp(window.location.hash));
-};
-
-window.addEventListener('load', init);
+myFunction();
