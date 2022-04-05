b0eef5e3992330b6a222b34f50ac5ae1eee4aca8
AnamariaMC <anymedinac14@gmail.com>
2022-04-05 16:13:35 -0500

    arreglos de los botones para salir del modal y guardar posts

--- src/lib/views/daily.js
+++ src/lib/views/daily.js
@@ -14,14 +14,14 @@ export default () => {
         <div id='modal_header'>
           <img id='user_img' src='./img/Icono_Harry.png'>
           <div id='name-container'>Wizard</div>
-          <i class="fa-solid fa-xmark" id="fa-solid"></i>
+          <i class="fa-solid fa-xmark" id="close"></i>
         </div>
         <div id='line'>
           <div id='text-container'>
             <textarea type='text' id='post-description' placeholder='Reveal your secrets'></textarea>
           </div>
         </div>
-        <button type='submit' id='btn-post-save' class='btn-post-inactive'>Save</button>  
+        <button disabled type='submit' id='btn-post-save' class='btn-post-inactive'>Save</button>  
       </form>
     </div>
     <div id='post-container' class="post-container">        
@@ -47,9 +47,6 @@ export default () => {
     
   })
 
-  // const modalForm = divDaily.querySelector('#modal_post-container');
-  
-
   const formPublication = divDaily.querySelector('#modal_post-container');
 
   formPublication.addEventListener('submit', (e) => {
@@ -60,7 +57,6 @@ export default () => {
     
   });
 
-  
   const postController = () => {
     const postContainer = divDaily.querySelector('#post-container');
     const querySnapshot = getPost();
@@ -82,45 +78,25 @@ export default () => {
   };
   postController();
 
+  // declaracion modalClose para evento de cierre de modal
+  let modalClose = divDaily.querySelector('#close'); 
+  modalClose.addEventListener('click',()=>{
+    console.log('Close');
+    background.style.display= "none";
+    modalPost.style.display= "";
+  });
 
-  
-  //readPost();
-  
-
-  // const btnSave = divDaily.querySelector('#btn-post-save');
-  // btnSave.addEventListener ('click', () => {
-    
-
-  // })
-
-
-
-  // btnSave.addEventListener('click', (e) => {
-  //   e.preventDefault()
-  //   console.log('Saved');
-  //   createPost(postDescription.value);
-  //   modalPost.reset();
-  // });
-
-
-  // // declaracion modalClose para evento de cierre de boton en version mobile
-  // let modalClose = divDaily.querySelector('#fa-solid'); 
-  // modalClose.addEventListener('click',()=>{
-  //   console.log('Close');
-  //   background.style.display= "none";
-  //   modalPost.style.display= "";
-  // });
-
-
-  // postDescription.addEventListener('keyup', () => { // evento del textarea
-  //   const postContent = postDescription.value.trim();
-  //   // trim() metodo que no permite activar boton con espacio
-  //   if (postContent === '') {
-  //     btnSave.disabled = true; // boton publicar inactivo
-  //   } else {
-  //     btnSave.disabled = false; // boton publicar activo
-  //   }
-  // });
+  // Función para no publicar espacios en blanco
+  const btnSave = divDaily.querySelector('#btn-post-save')
+  postDescription.addEventListener('keyup', () => { // evento del textarea
+    const postContent = postDescription.value.trim();
+    // trim() metodo que no permite activar boton con espacio
+    if (postContent === '') {
+      btnSave.disabled = true; // boton publicar inactivo
+    } else {
+      btnSave.disabled = false; // boton publicar activo
+    }
+  });
 
   // const postController = () => {
   //   const postContainer = divDaily.querySelector('#post-container');
@@ -146,6 +122,14 @@ export default () => {
   // };
   // postController();
   
+  
+
+  // btnSave.addEventListener('click', (e) => {
+  //   e.preventDefault()
+  //   console.log('Saved');
+  //   createPost(postDescription.value);
+  //   modalPost.reset();
+  // });
   
   return divDaily;
 };
