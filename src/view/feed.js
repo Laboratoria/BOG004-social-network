18d0da8e55d1a450a84068fa972db3ea9bfc2966
Mafe valencia <mariafvc22@gmail.com>
2022-04-05 19:27:38 -0500

    agrego textos para cada uno de los íconos

--- src/view/feed.js
+++ src/view/feed.js
@@ -57,10 +57,11 @@ export default () => {
       <hr>
       <p>${recipe.description}</p>
       <div class= 'iconos'>
-      <img id= 'likeImg' src='../img/like.png' alt='like_image'>
-      <img id= 'espatulaImg' src='../img/espatula.png' alt='espatula_image'>
-      <img id= 'rodilloImg' src='../img/rodillo.png' alt='rodillo_image'>
+      <img id= 'likeImg' src='../img/like.png' alt='like_image'><p class='likeText'>Me gusta</p>
+      <img id= 'espatulaImg' src='../img/espatula.png' alt='espatula_image'><p class='eliminarText'>Eliminar</p>
+      <img id= 'rodilloImg' src='../img/rodillo.png' alt='rodillo_image'><p class='editarText'>Editar</p>
       </div>
+      
     </div>`;
       const recipesContainer = divFeed.querySelector('#container-recipes');
       recipesContainer.innerHTML = recipesToShow;
