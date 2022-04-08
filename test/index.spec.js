4cb1bacf1d20c12b539a878b5dfab2307cc41676
Mafe valencia <mariafvc22@gmail.com>
2022-04-08 12:27:47 -0500

    ajustes código

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,10 @@
 import { changeView } from '../src/view-controler/router';
+
 jest.mock('../src/lib/firebase-utils.js');
 document.body.innerHTML = '<div id="sectionGrid"><section id="container"></section></div>';
 describe('changeView y login', () => {
   it('debería mostrar la vista login', () => {
     changeView('#/login');
-    expect(document.getElementById('container').textContent).toContain('¡Holaaaa!');
+    expect(document.getElementById('container').textContent).toContain('¡Hola!');
   });
 });
