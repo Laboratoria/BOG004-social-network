745130090eb2ca06aee1f3497b526aab2bfb6924
Paola Huyo <di.paolahuyo@gmail.com>
2022-04-12 11:32:58 -0500

    recibi los cambios de yuli del 12 de Abril

--- src/Firebase/__ mocks__/firebase-import.js
+++
@@ -1,6 +0,0 @@
-export const getAuth = () => Promise.resolve({});
-export const createUserWithEmailAndPassword = () => Promise.resolve({});
-export const sendEmailVerification = () => Promise.resolve({});
-export const signInWithEmailAndPassword = () => Promise.resolve({});
-export const onAuthStateChanged = () => Promise.resolve({});
-export const getFirestore = () => Promise.resolve({});
